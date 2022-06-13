9a3a943be59bfb607bc89680156bb3e2bddb5598
JoaquinFloress <joaquinnfloress16@gmail.com>
2022-06-13 20:59:30 -0300

    first commit

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-const {Client, Intents} = require('discord.js')
+const {Client, Intents, Collection} = require('discord.js')
 require('dotenv').config()
 const config = require('./config.json')
 const i18n = require('i18n')
+const fs = require('fs')
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS]})
+
+client.commands = new Collection()
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`)
+    client.commands.set(command.data.name, command)
+}
 
 client.once('ready', () =>{
     console.log('Bot is ready')
@@ -12,10 +21,23 @@ client.once('ready', () =>{
 client.on('interactionCreate', async interaction =>{
     if (!interaction.isCommand()) return
 
-    const { commandName } = interaction
-    if (commandName == 'pìng'){
-        await interaction.reply('Pong!');
+    const command = client.commands.get(interaction.commandName)
+    if (!command) return
+
+    try{
+        await command.execute(interaction)
+    }catch(e){
+        console.error(e)
+        return interaction.reply({ content: "Surgio un error porque te olvidaste algo gil." })
     }
+
+
+
+
+    // const { commandName } = interaction
+    // if (commandName == 'pìng'){
+    //     await interaction.reply('Pong!');
+    // }
 })
 
 //client.login(process.env.token)
--- slashcommands.js
+++ slashcommands.js
@@ -1,14 +1,19 @@
-const { SlashCommandBuilder } = require('@discordjs/builders')
+//const { SlashCommandBuilder } = require('@discordjs/builders')
+const fs = require('fs')
 const { REST } = require('@discordjs/rest')
 const { Routes } = require('discord-api-types/v9')
 const { clientId, guildId, token } = require('./config.json')
 
 const commands = [ 
-    new SlashCommandBuilder().setName('ping').setDescription('Pingueame esta')
-
+    //new SlashCommandBuilder().setName('ping').setDescription('Pingueame esta')
 ]
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
+//.map(command => command.toJSON())
 
-.map(command => command.toJSON())
+for (const file of commandFiles){
+    const command= require(`./commands/${file}`)
+    commands.push(command.data.toJSON())
+}
 
 const rest = new REST({ version: '9'}).setToken(token)
 
